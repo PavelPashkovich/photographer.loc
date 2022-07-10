588891599b7c1f8a33c5c68b4a8c63f022a043af
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-07-10 12:06:14 +0300

    fix main city view

--- resources/views/main/city/index.blade.php
+++ resources/views/main/city/index.blade.php
@@ -10,7 +10,7 @@
                         @foreach($cities as $city)
                             <div class="col-md-12 col-lg-6 col-xl-4">
                                 <div class="card mb-2 bg-gradient-dark">
-                                    @if(isset($city->users->first()->photos->first()->photo) && !empty($city->users->first()->photos->first()->photo))
+                                    @if(isset($city->users->first()->photos) && !empty($city->users->first()->photos))
                                         <img class="card-img-top" src="@if(isset($city->users->first()->photos->first()->photo)){{ asset('storage/'.$city->users->first()->photos->first()->photo) }}@else{{ asset('storage/city.jpg') }}@endif" alt="{{ $city->name }}">
                                     @else
                                         <img class="card-img-top" src="{{ asset('storage/city.jpg') }}" alt="{{ $city->name }}">

733e67d963c195dd5dba2928cc2d4957b0d2474d
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-07-10 17:03:57 +0300

    fix profile photo show

--- resources/views/profile/photo/show.blade.php
+++ resources/views/profile/photo/show.blade.php
@@ -38,7 +38,7 @@
             <div class="card card-widget">
                 <div class="card-header">
                     <div class="user-block">
-                        <img class="img-circle" src="{{ asset('storage/'.$photo->user->avatar) }}" alt="User Image">
+                        <img class="img-circle" src="@if(isset($photo->user->avatar)){{ asset('storage/'.$photo->user->avatar) }}@else{{ asset('storage/avatars/noavatar.jpg') }}@endif" alt="User Image">
                         <span class="username text-danger">{{ $photo->user->name }}</span>
                         <span class="description">Shared publicly - {{ $photo->created_at }}</span>
                     </div>

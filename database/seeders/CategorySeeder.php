3c88fb32959922b58c67119ad469ad6db66d9837
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-07-11 22:01:39 +0300

    add category seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('cities')->insert([
+            [
+                'name' => 'Landscape',
+                'slug' => 'landscape',
+            ],
+            [
+                'name' => 'Portrait',
+                'slug' => 'portrait',
+            ],
+            [
+                'name' => 'Kids',
+                'slug' => 'kids',
+            ],
+            [
+                'name' => 'Wedding',
+                'slug' => 'wedding',
+            ],
+            [
+                'name' => 'Macro',
+                'slug' => 'macro',
+            ],
+            [
+                'name' => 'Nature',
+                'slug' => 'nature',
+            ],
+            [
+                'name' => 'City / Architecture',
+                'slug' => 'city-architecture',
+            ],
+            [
+                'name' => 'Street / Reportage',
+                'slug' => 'street-reportage',
+            ],
+            [
+                'name' => 'Family portrait',
+                'slug' => 'family-portrait',
+            ],
+            [
+                'name' => 'Amimals',
+                'slug' => 'animals',
+            ],
+            [
+                'name' => 'Still life',
+                'slug' => 'still-life',
+            ],
+            [
+                'name' => 'Black and white',
+                'slug' => 'black-and-white',
+            ],
+            [
+                'name' => 'Sport',
+                'slug' => 'sport',
+            ],
+            [
+                'name' => 'Night',
+                'slug' => 'night',
+            ],
+            [
+                'name' => 'Film',
+                'slug' => 'film',
+            ],
+        ]);
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,11 +17,46 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Pavel Pashkovich',
-            'email' => 'pashkovich.pavel@gmail.com',
-            'password' => Hash::make(12345678),
-            'city_id' => 5,
-            'role_id' => 1,
+            [
+                'name' => 'Pavel Pashkovich',
+                'slug' => 'pavel-pashkovich',
+                'email' => 'pashkovich.pavel@gmail.com',
+                'password' => Hash::make(12345678),
+                'city_id' => 5,
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Andrey',
+                'slug' => 'andrey',
+                'email' => 'andrey@gmail.com',
+                'password' => Hash::make(12345678),
+                'city_id' => 1,
+                'role_id' => 2,
+            ],
+            [
+                'name' => 'Mark',
+                'slug' => 'mark',
+                'email' => 'mark@gmail.com',
+                'password' => Hash::make(12345678),
+                'city_id' => 2,
+                'role_id' => 2,
+            ],
+            [
+                'name' => 'Nasty',
+                'slug' => 'nasty',
+                'email' => 'nasty@gmail.com',
+                'password' => Hash::make(12345678),
+                'city_id' => 3,
+                'role_id' => 2,
+            ],
+            [
+                'name' => 'Tanya',
+                'slug' => 'tanya',
+                'email' => 'tania@gmail.com',
+                'password' => Hash::make(12345678),
+                'city_id' => 4,
+                'role_id' => 2,
+            ],
         ]);
     }
 }
